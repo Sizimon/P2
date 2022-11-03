b61b8b3b8d904c534002365c6400a8fa1142dada
Sizimon <siizmonsus@gmail.com>
2022-11-03 18:58:04 +0000

    Feat: added template for history of games

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
 const choiceButtons = document.querySelectorAll('[data-choice]');
+const finalColumn = document.querySelector('[data-column]')
 const CHOICES = [
     {
         name: 'rock',
-        image: './assets/images/rock.png',
+        image: './assets/images/rock',
         beats: 'scissors'
     },
     {
@@ -20,10 +21,35 @@ const CHOICES = [
 choiceButtons.forEach(choiceButton => {
     choiceButton.addEventListener('click', event => {
         const choiceName = choiceButton.dataset.choice
-        makeChoice(choiceName);
+        const choice = CHOICES.find(choice => choice.name === choiceName)
+        makeChoice(choice);
     })
 })
 
+
+
 function makeChoice(choice) {
-    console.log(choice);
+    const computerChoice = randomChoice();
+    const youWin = isWinner(choice, computerChoice);
+    const computerWin = isWinner(computerChoice, choice);
+    addChoiceResult(computerChoice, computerWin);
+    addChoiceResult(choice, youWin);
+}
+
+function addChoiceResult(choice, winner) {
+    const div = document.createElement('div');
+    div.innerText = choice.image;
+    div.classList.add('result-choice');
+    if (winner) div.classList.add('winner');
+    finalColumn.after(div);
+    
+}
+
+function randomChoice() {
+    const randomIndex = Math.floor(Math.random() * CHOICES.length);
+    return CHOICES[randomIndex];
+}
+
+function isWinner(choice, computerChoice) {
+    return choice.beats === computerChoice.name
 }
